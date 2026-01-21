8c9db8c2ab48e15d046ff52c6e3b09a26866ef42
Itay Maman <94941+imaman@users.noreply.github.com>
2026-01-22 00:32:47 +0200

    inherit

--- src/publish.ts
+++ src/publish.ts
@@ -6,7 +6,7 @@ import type { AbsolutePath } from './paths.js'
 export function publish(outputDir: AbsolutePath, monorepoRoot: AbsolutePath) {
   const npmrcPath = path.join(monorepoRoot, '.npmrc')
   const userconfigArg = fs.existsSync(npmrcPath) ? ` --userconfig ${npmrcPath}` : ''
-  execSync(`npm publish${userconfigArg}`, { cwd: outputDir, stdio: 'pipe', encoding: 'utf-8' })
+  execSync(`npm publish${userconfigArg}`, { cwd: outputDir, stdio: 'inherit', encoding: 'utf-8' })
   // if (npmPublishResult.status !== 0) {
   //   throw new Error(`npm publish failed with exit code ${String(npmPublishResult.status ?? 1)}`)
   // }
