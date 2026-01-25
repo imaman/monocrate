d339f15a990f23d936556152a0035f848e8c9c09
Itay Maman <94941+imaman@users.noreply.github.com>
2026-01-25 08:57:03 +0200

    dist/index.js

--- src/testing/monocrate-teskit.ts
+++ src/testing/monocrate-teskit.ts
@@ -26,7 +26,7 @@ export function pj(name: string, version = '1.0.0', more: Partial<PackageJson> =
   // TODO(imaman): type "module" by default?
   return {
     // TODO(imaman): align with index.mjs
-    main: 'index.js',
+    main: 'dist/index.js',
     ...more,
     name,
     version,
